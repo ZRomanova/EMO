8fa273ebb3be2282482561d73c09bae54d76a126
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-26 21:00:18 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello, Zoya!' });
 });
 
+/* Сраницы полной версии. */
+router.get('/full', ctrlFull)
+
+/* Страницы лайт-версии. */
+router.get('/friends', ctrlLite)
+router.get('/friends/libary', ctrlLite)
+router.get('/friends/libary/questions', ctrlLite)
+router.get('/friends/libary/ansers', ctrlLite)
+router.get('/friends/libary/stickers', ctrlLite)
+router.get('/friends/libary/downloads', ctrlLite)
+router.get('/friends/seach', ctrlLite)
+router.get('/friends/settings', ctrlLite)
+
 module.exports = router;
