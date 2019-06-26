4f2a07d66c8eac259efc60ccb68360ffc7a4f072
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-27 00:53:59 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+/*
 var ctrlLite = require('../controllers/lite');
-var ctrlFull = require('../controllers/full');   
+var ctrlFull = require('../controllers/full'); 
+Надо ли это?
+*/
 
-/* GET home page. */
+/* GET home page. Тут ли оно?*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello, Zoya!' });
 });
