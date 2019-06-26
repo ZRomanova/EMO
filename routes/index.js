3b693e5641061766f8444b43dc02fe9de25e844d
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-26 20:37:52 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var ctrlLite = require('../controllers/lite');
+var ctrlFull = require('../controllers/full');   
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
