52969a50e3aedd74b18b4e12613653c5caecab70
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-27 11:42:03 +0300

    Update full.js

--- controllers/full.js
+++ controllers/full.js
@@ -1,9 +1,9 @@
 /* GET home page. */
-router.get('/', function(req, res, next) {
+module.exports.getHomePage = function(req, res, next) {
   res.render('index', { title: 'Регистрация!' });
 });
 
 /* Получить страницу полной версии. */
-router.get('/full', function(req, res, next) {
+module.exports.getFullVersion = function(req, res, next) {
   res.render('index', { title: 'Тут полная версия!' });
 });
