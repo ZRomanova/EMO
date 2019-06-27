9f906ce8b4cd38baa50a96b23b8fd249f2236c9a
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-27 20:45:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('../../routes/index.js');
-var usersRouter = require('../../routes/users.js');
+var indexRouter = require('../app_server/routes/index.js');
+var usersRouter = require('../app_server/routes/users.js');
 var app = express();
 
 // view engine setup
