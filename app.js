adbf7a58a7a950739f7db0a1706f1cceb37d2b9e
Zoya Romanova <40735669+ZRomanova@users.noreply.github.com>
2019-06-27 23:03:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var createError = require('node_modules/http-errors');
-var express = require('node_modules/express');
+var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
