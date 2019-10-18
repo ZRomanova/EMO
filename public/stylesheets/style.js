4f48737de72a83c1935c130cb542cd902dbaa813
Zoya <romanova.zoya.2002@mail.ru>
2019-10-18 23:22:24 +0300

    Commit message.

--- public/stylesheets/style.js
+++ public/stylesheets/style.js
@@ -1,16 +1,18 @@
-function text_color(Id, Value) {
+var textfunction = window.onload 
+textfunction = function text_color() {
 alert ('start')
-var mas = document.getElementById(Id).innerHTML.split("");
+var mas = document.getElementsByClassName('TC').innerHTML.split("");
 var my_text = Value.length-1;
 var count = 0;
 var letter = mas[count];
-while count <= my_text {
+while (count <= my_text) {
   alert(count,letter)
   letter = letter.toUpperCase()
-  if letter = 'А' || 'О' || 'У' || 'Ы' || 'Э' || 'Я' || 'Ё' || 'Ю' || 'И' || 'Е' {
+  if (letter = 'А' || 'О' || 'У' || 'Ы' || 'Э' || 'Я' || 'Ё' || 'Ю' || 'И' || 'Е') {
     mas[count] = "<font color = red>" + mas[count] + "</font>";
     count++;
-}}  
-document.getElementById(id).innerHTML = mas.join("");
+  }}  
+document.getElementById(Id).innerHTML = mas.join("");
 }
 // запуск onload = 'text_color(id, value)'
+
