9e5e366317fa902dc8f404d7ae9155c1ef2d9165
Zoya <romanova.zoya.2002@mail.ru>
2019-10-20 15:17:51 +0300

    Commit message.

--- app_server/models/libraries.js
+++ app_server/models/libraries.js
@@ -1,21 +1,26 @@
 var mongoose = require( 'mongoose' );
+
 var questionsSchema = new mongoose.Schema({
-    imgUrl: {type: String, required: true}
+    img: {type: Buffer, required: true}
 });
 
 var answersSchema = new mongoose.Schema({
-    imgUrl: {type: String, required: true}
+    img: {type: Buffer, required: true}
 });
 
 var stickersSchema = new mongoose.Schema({
-    imgUrl: {type: String, required: true}
+    img: {type: Buffer, required: true}
 });
 
 var downloadsSchema = new mongoose.Schema({
-    imgUrl: {type: String, required: true}
+    img: {type: Buffer, required: true}
 });
 
-mongoose.model('Questions', questionsSchema, 'Libraries');
-mongoose.model('Answers', answersSchema, 'Libraries');
-mongoose.model('Stickers', stickersSchema, 'Libraries');
-mongoose.model('Downloads', downloadsSchema, 'Libraries');
+var librariesSchema = new mongoose.Schema({
+    questions: [questionsSchema],
+    answers: [answersSchema],
+    stickers: [stickersSchema],
+    downloads: [downloadsSchema]
+});
+
+mongoose.model('Library', librariesSchema, 'Libraries');
