85ce8a05eadc7ad0edef0b49dea97ab7aa2f4be3
Zoya <romanova.zoya.2002@mail.ru>
2019-10-21 19:59:29 +0300

    Commit message.

--- public/stylesheets/style.js
+++ public/stylesheets/style.js
@@ -1,18 +1,23 @@
-var textfunction = window.onload 
-textfunction = function text_color() {
-alert ('start')
-var mas = document.getElementsByClassName('TC').innerHTML.split("");
-var my_text = Value.length-1;
-var count = 0;
-var letter = mas[count];
-while (count <= my_text) {
-  alert(count,letter)
-  letter = letter.toUpperCase()
-  if (letter = 'А' || 'О' || 'У' || 'Ы' || 'Э' || 'Я' || 'Ё' || 'Ю' || 'И' || 'Е') {
-    mas[count] = "<font color = red>" + mas[count] + "</font>";
-    count++;
-  }}  
-document.getElementById(Id).innerHTML = mas.join("");
-}
-// запуск onload = 'text_color(id, value)'
+window.onload = function() {
+for (let i = 0; ;i++) {
+    if (document.getElementsByClassName('tc')[i] == undefined) break;
+    else {
+        mas = document.getElementsByClassName('tc')[i].innerHTML;
+        var my_text = mas.length-1;
+        mas = mas.split([''])
+        var count = 0;
+        var fin = '';
+        while (count <= my_text) {
+            var letter = mas[count];
+            letter = letter.toUpperCase();
+            if (letter == 'А' || letter ==  'О' || letter == 'У' || letter == 'Ы' || letter == 'Э' || letter == 'Я' || letter == 'Ё' || letter == 'Ю' || letter == 'И' || letter == 'Е') {
+                letter = "<span class = red>" + letter + "</span>";
+            };
+            fin = fin + letter;
+            count++;
+        };   
+    document.getElementsByClassName('tc')[i].innerHTML = fin;
+    };
+};
+};
 
